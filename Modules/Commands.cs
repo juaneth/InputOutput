92ec23803f9416aafde9771aff9ea430ff5b394a
Juan <euanwalker08@outlook.com>
2021-07-29 20:29:23 +0100

    added cmd command

--- Modules/Commands.cs
+++ Modules/Commands.cs
@@ -3,9 +3,10 @@ using Discord.Commands;
 using Discord;
 using System.IO;
 using System.IO.Compression;
-using System;
-using System.Threading;
 using Discord.WebSocket;
+using Discord.Audio;
+using System.Diagnostics;
+using System;
 
 namespace InputOutput.Modules
 {
@@ -21,8 +22,10 @@ namespace InputOutput.Modules
             };
 
             HelpEmbed.AddField("io.RT/io.retrieve",
-            "This 'retrieves' a file or folder and sends it as a file!").WithAuthor(Context.Client.CurrentUser).WithFooter(footer => footer.Text = ":)").WithColor(Color.DarkerGrey).WithDescription("This command was called by " + Context.Message.Author).WithCurrentTimestamp();
-
+            "This 'retrieves' a file or folder and sends it as a file!").AddField("io.tree",
+            "This lists all file names in a certain directory!").AddField("io.cmd",
+            "This runs whatever command the user has sent in command prompt!").WithAuthor(Context.Client.CurrentUser).WithFooter(footer => footer.Text = ":)").WithColor(Color.DarkerGrey).WithDescription("This command was called by " + Context.Message.Author).WithCurrentTimestamp();
+            
 
 
             await ReplyAsync(embed: HelpEmbed.Build());
@@ -61,8 +64,8 @@ namespace InputOutput.Modules
                 var messages = await Context.Message.Channel.GetMessagesAsync(1).FlattenAsync();
                 await (Context.Channel as SocketTextChannel).DeleteMessagesAsync(messages);
 
-                var sendfilesent = filesent.Build();
-                await Context.Channel.SendFileAsync(embed: sendfilesent, filePath: filelocal);
+                await ReplyAsync(embed: filesent.Build());
+                await Context.Channel.SendFileAsync(filelocal);
             }
             else
             {
@@ -93,7 +96,8 @@ namespace InputOutput.Modules
                         var messages = await Context.Message.Channel.GetMessagesAsync(1).FlattenAsync();
                         await (Context.Channel as SocketTextChannel).DeleteMessagesAsync(messages);
 
-                        await Context.Channel.SendFileAsync(zipped, "Here you go!");
+                        await ReplyAsync(embed: filesent.Build());
+                        await Context.Channel.SendFileAsync(filelocal);
                     }
                 }
             }
@@ -167,6 +171,44 @@ namespace InputOutput.Modules
 
                         await Context.Channel.SendFileAsync(zipped, "Here you go!");
                     }
+                }
+            }
+        }
+
+        [Command("cmd")]
+        [RequireBotPermission(GuildPermission.SendMessages)]
+        [RequireBotPermission(GuildPermission.AttachFiles)]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task CMD(string commandinput,[Remainder]string workingdir)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo("cmd.exe");
+            psi.RedirectStandardOutput = true;
+            psi.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            psi.UseShellExecute = false;
+            psi.Arguments = "/C " + commandinput;
+            psi.WorkingDirectory = workingdir;
+            Process proc = Process.Start(psi); ////
+            StreamReader myOutput = proc.StandardOutput;
+            proc.WaitForExit(6000);
+            if (proc.HasExited)
+            {
+                string output = myOutput.ReadToEnd();
+
+                if (output.Length > 2000)
+                {
+                    if (File.Exists("output.txt"))
+                    {
+                        File.Delete("output.txt");
+                    }
+
+                    string createText = "This output was too large for Discord, so we sent it as a .txt file" + Environment.NewLine;
+                    File.WriteAllText("output.txt", createText + "\n \" + output");
+
+                    await Context.Channel.SendFileAsync("output.txt");
+                }
+                else
+                {
+                    await ReplyAsync(output);
                 }
             }
         }
